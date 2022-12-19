61d58b093ce673003c1a86d67f26d3810706d677
Alex Dolhov <alexdolhov@gmail.com>
2022-12-19 19:22:07 +0200

    adds all the code

--- contacts.js
+++ contacts.js
@@ -1,26 +1,66 @@
 const fs = require('fs').promises;
-const path = require('node:path');
+const path = require('path');
 
-const contactsPath = path.basename('/db/contacts.json');
+// const contactsPath = path.resolve('./db/contacts.json');
+// const contactsPath = `${__dirname}/db/contacts.json`;
+const contactsPath = path.join(__dirname, '/db/contacts.json');
 
 async function listContacts() {
   try {
     const contacts = await fs.readFile(contactsPath, 'utf-8');
-    console.table(JSON.parce(contacts));
-    return JSON.parce(contacts);
+    // console.table(JSON.parse(contacts));
+    // return JSON.parse(contacts);
+    console.table(JSON.parse(contacts));
   } catch (error) {
     console.log(error);
   }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const contacts = await fs.readFile(contactsPath, 'utf-8');
+    const parcedContacts = JSON.parse(contacts);
+    const contact = parcedContacts.find(contact => contact.id === contactId);
+    console.table(contact);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const contacts = await fs.readFile(contactsPath, 'utf-8');
+    const parcedContacts = JSON.parse(contacts);
+    const leftContacts = parcedContacts.filter(
+      contact => contact.id !== contactId
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(leftContacts), 'utf-8');
+    listContacts();
+    // console.table(parcedContacts);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await fs.readFile(contactsPath, 'utf-8');
+    const parcedContacts = JSON.parse(contacts);
+    const addContact = [
+      ...parcedContacts,
+      {
+        id: (parcedContacts.length + 1).toString(),
+        name,
+        email,
+        phone,
+      },
+    ];
+    await fs.writeFile(contactsPath, JSON.stringify(addContact), 'utf-8');
+    console.table(parcedContacts);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
